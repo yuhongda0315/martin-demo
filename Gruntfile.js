f56bbfb61d1fd41fdfd08502f5d9303b6e728b44
yuhongda <yuhongda@rongcloud.cn>
2016-11-25 11:00:15 +0800

    uploader

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt){
       connect: {
         options: {
                 hostname: '127.0.0.1', //默认就是这个值，可配置为本机某个 IP，localhost 或域名 wifi 10.10.112.95  wlan:10.10.130.50
-                livereload: 35056  //声明给 watch 监听的端口
+                livereload: 35050  //声明给 watch 监听的端口
             },
          server: {
            options: {
---
+++ im/DataModel.js
@@ -0,0 +1,23 @@
+(function(){
+
+	var Utils = {
+
+	};
+
+	var Conversation = {
+		data : [],
+		get : function(){ },
+		set : function(){ },
+		change : function(){ }
+	};
+
+	var RealTimeMessage = {
+		data : {
+			'conversationType_targetId' : []
+		},
+		get : function(){ },
+		set : function(){ },
+		change : function(){}
+	};
+
+})();
