3bfa0a2b7f96c06aca123d00ff3fef780d4f6527
yuhongda <yuhongda@rongcloud.cn>
2016-11-30 19:11:36 +0800

    update

--- im/DataModel.js
+++ im/DataModel.js
@@ -1,24 +1,97 @@
-(function(){
+(function(win){
+	var opts = {
+		max_msg_count:20,
+		max_conversation_count:50,
+		get_historyMsg_count:20
+	};
+
+	var Listener = {
+		onStatusChange:function(status){ },
+		onMessage:function(message){ },
+		onError:function(error, message){ }
+	};
+ 
+	var sendMessage = function(conversationType, targetId, content){
+		RongIMClient.getInstance().sendMessage(conversationType, targetId, content, {
+                onSuccess: function (message) {
+                   Listener.onMessage(message);
+                },
+                onError: function (errorCode,message) {
+                   Listener.onError(errorCode, message); 
+                }
+            }
+        );
+	};
+
+	var connect = function(token){
+		RongIMClient.connect(token, {
+	        onSuccess: function(userId) {
+	        
+	        }, 
+	        onTokenIncorrect: function() {
+	        	Listener.onStatusChange(RongIMLib.ConnectionState.TOKEN_INCORRECT);
+	        },
+	        onError:function(errorCode){
+	        	Listener.onStatusChange(errorCode);
+	        }
+	    });
+	};
+
+	var disconnect = function(){
+		RongIMClient.getInstance().disconnect();
+	};
+
+	var init = function(appkey, dataAccessProvider, options){
+
+		API.getUsers(function(users){
+    		User.set(users);
+    	});
+		API.getFriendShip(function(data){
+			FriendShip.set(data);
+		});
+		API.getGroups(function(data){
+			Group.set(data);
+		});
+
+		RongIMClient.init(appkey, dataAccessProvider, options);
+
+		RongIMClient.setConnectionStatusListener({
+		    onChanged: function (status) {
+		    	Listener.onStatusChange(status);
+		    }
+		});
+
+		RongIMClient.setOnReceiveMessageListener({
+		    onReceived: function (message) {
+		        Listener.onMessage(message);
+		    }
+		});
+
+		return {
+			User:User,
+			Group:Group,
+			GroupMembers:GroupMembers,
+			FriendShip:FriendShip,
+			Conversation:Conversation,
+			Message:Message,
+			Discussion:Discussion,
+			ChatRoom:ChatRoom,
+			Status:Status,
+			sendMessage:sendMessage,
+			connect:connect,
+			disconnect:disconnect
+		}
+	};
 
 	var Util = {
-		opts:{ 
-			max_msg_count:20,
-			max_conversation_count:50,
-			get_historyMsg_count:20
-		},
 		getPrototype:Object.prototype.toString,
 		forEach:function(obj,callback){
 			for(var key in obj){
 				callback(key,obj[key]);
 			}
 		},
-		createConvresation:function(message){
-			var conversation = RongIMClient._memoryStore.conversationList.slice(0,1)[0];
-			conversation.senderUserInfo = message.senderUserInfo;
-			return conversation;
-		},
 		composeObject:function(data, callback){
-			if (Util.getPrototype.call(data) == "[object Array]") {
+			if (this.getPrototype.call(data) == "[object Array]") {
 				var index = 0, items = [];
 				var invoke = function(){
 					if (index == data.length) {
@@ -41,21 +114,21 @@
 		},
 		composeUserInfo:function(data, callback){
 			if (data.conversationType == RongIMLib.ConversationType.PRIVATE) {
-				RongIM.User.get(data.targetId || data.senderUserId,function(user){
+				User.get(data.senderUserId || data.targetId,function(user){
 					data.senderUserInfo = user;
 					callback(data);
 				});
 			}else if(data.conversationType == RongIMLib.ConversationType.GROUP){
 				if (data.latestMessage) {
-					RongIM.Group.get(function(group){
+					Group.get(function(group){
 						data.senderUserInfo = group;
 						callback(data);
 					},data.targetId);
 				}else{
-					RongIM.Group.get(function(group){
-						data.senderUserInfo = group;
+					User.get(data.senderUserId,function(user){
+						data.senderUserInfo = user;
 						callback(data);
-					},data.senderUserId);					
+					});
 				}
 			}else{
 				data.senderUserInfo = {};
@@ -64,101 +137,92 @@
 		}
 	};
 
-	function RongIM(appkey, dataAccessProvider, options){ 
-		RongIMLib.RongIMClient.init(appkey, dataAccessProvider, options);
-
-		RongIMClient.setConnectionStatusListener({
-		    onChanged: function (status) {
-		    	RongIM.Status.set(status);
-		    }
-		});
-
-		RongIMClient.setOnReceiveMessageListener({
-		    onReceived: function (message) {
-		        switch(message.messageType){
-		            case RongIMClient.MessageType.TextMessage:
-		                break;
-		            case RongIMClient.MessageType.VoiceMessage:
-		                break;
-		            case RongIMClient.MessageType.ImageMessage:
-		               break;
-		            case RongIMClient.MessageType.DiscussionNotificationMessage:
-		               break;
-		            case RongIMClient.MessageType.LocationMessage:
-		               break;
-		            case RongIMClient.MessageType.RichContentMessage:
-		               break;
-		            case RongIMClient.MessageType.InformationNotificationMessage:
-		               break;
-		            case RongIMClient.MessageType.ContactNotificationMessage:
-		               break;
-		            case RongIMClient.MessageType.ProfileNotificationMessage:
-		               break;
-		            case RongIMClient.MessageType.CommandNotificationMessage:
-		               break;
-		            case RongIMClient.MessageType.CommandMessage:
-		               break;
-		            case RongIMClient.MessageType.UnknownMessage:
-		               break;
-		            default:
-		        }
-		        RongIM.Message.set(message);
-		    }
-		});
-	}
-
-	RongIM.init = function(appkey, dataAccessProvider, options){
-		if (RongIM._instance) {
-			return RongIM._instance;
-		}else{
-			RongIM._instance = new RongIM(appkey, dataAccessProvider, options);
-		}
-		RongIM.API.getUsers(function(users){
-    		User.set(users);
-    	});
-		RongIM.API.getFriendShip(function(data){
-			FriendShip.set(data);
-		});
-		RongIM.API.getGroups(function(data){
-			Group.set(data);
-		});
-
-	};
-
-	RongIM.getInstance = function(){
-		return RongIM._instance;
-	};
-
-	RongIM.prototype.sendMessage = function(conversationType, targetId, content){
-
-	};
-
-	RongIM.prototype.connect = function(token){
-		RongIMClient.connect(token, {
-	        onSuccess: function(userId) {
-	        
-	        },
-	        onTokenIncorrect: function() {
-	        	RongIM.Status.set(RongIMLib.ConnectionState.TOKEN_INCORRECT);
-	        },
-	        onError:function(errorCode){
-	        	RongIM.Status.set(errorCode);
+	var API = { 
+		getUserInfo:function(data, userId, callback){
+			if (userId in data) {
+				callback(data[userId]);
+			}else{
+				var defaultUser = {
+					name:'陌生人',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/Uz6Sw8GXx1476068767254905029'
+				};
+				var users = {'1002':{id:'1002',name:'小企鹅',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/675NdFjkx1466733699776768066'},
+							'1003':{id:'1003',name:'飞翔的企鹅',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/FjsNMjYoVKfGmA86SNwnggfKgE6_'},
+							'1004':{id:'1004',name:'CPU',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/Tp6nLyUKX1466570511241467041'},
+							'1005':{id:'1005',name:'西湖龙井',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/Fh4fnCvnO_SOwpuMPYGBnzBwrx6A'},
+							'1001':{id:'1001',name:'大海飞',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/u0LUuhzHm1466557920584458984'}}
+				callback(users[userId] || defaultUser);
+			}
+		},
+		getUsers:function(callback){},
+		getFriendShip : function(callback){ },
+		getGroups:function(callback){ },
+		getGroupMembers:function(callback){ },
+		getGroupInfo:function(data, groupId, callback){ 
+			if (data.length > 0) {
+				for(var i=0,len = data.length; i<len; i++){
+					if (data[i].group.id == groupId) {
+						callback(data[i]);
+						break;
+					}
+				}
+			}else{
+				callback({id:'"daGroup9901"',name:'RongCloud',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/Tp6nLyUKX1466570097727076172'});
+			}
+		},
+		getConversationList : function(callback){ 
+			RongIMClient.getInstance().getConversationList({
+				 onSuccess: function(result) {
+				  	Util.composeObject(result,function(items){
+				  		var list = API.sortConversationList(items);
+				  		if (list.length > opts.max_conversation_count) list.length = opts.max_conversation_count;
+						callback(list);
+					});
+				  },
+				  onError: function(error) { }
+			},[1,3]);
+		},
+		
+		sortConversationList : function(conversationList, callback){ 
+			var convers = [];
+	        for (var i = 0, len = conversationList.length; i < len; i++) {
+	            if (!conversationList[i]) {
+	                continue;
+	            }
+	            if (conversationList[i].isTop) {
+	                convers.push(conversationList[i]);
+	                conversationList.splice(i, 1);
+	                continue;
+	            }
+	            for (var j = 0; j < len - i - 1; j++) {
+	                if (conversationList[j].sentTime < conversationList[j + 1].sentTime) {
+	                    var swap = conversationList[j];
+	                    conversationList[j] = conversationList[j + 1];
+	                    conversationList[j + 1] = swap;
+	                }
+	            }
 	        }
-	    });
-	}
-
-	RongIM.prototype.disconnect = function(){
-		RongIMClient.getInstance().disconnect();
+	        return convers.concat(conversationList);
+		},
+		removeConversation:function(conversationType, targetId, callback){},
+		getHistoryMessages : function(conversationType, targetId, timestamp, callback){ 
+			RongIMLib.RongIMClient.getInstance().getHistoryMessages(conversationType, targetId, timestamp, opts.get_historyMsg_count, {
+			  onSuccess: function(list, hasMore) {
+			  	callback(list, hasMore);
+			  },
+			  onError: function(error) { }
+			});
+		}
 	};
+
 	/**
 		data:{ }
 		set: users 格式： { userId1 : {id:'', name:'', portraitUri:''}, userId2 : {id:'', name:'', portraitUri:''}  }
 	*/
-	RongIM.User = {
+	var User = {
 		data:{},
 		get:function(userId,callback){
 			var me = this;
-			RongIM.API.getUserInfo(me.data, userId, function(user){
+			API.getUserInfo(me.data, userId, function(user){
 				me.data[userId] = user;
 				callback(user);
 			});
@@ -172,7 +236,7 @@
 		}
 	};
 	
-	RongIM.FriendShip = { 
+	var FriendShip = { 
 		data:[],
 		get:function(){
 			return this.data;
@@ -184,9 +248,9 @@
 		onChange:function(friends){ }
 	};
 
-	RongIM.Discussion = { };
+	var Discussion = { };
 
-	RongIM.ChatRoom = {};	
+	var ChatRoom = { };	
 
 	/**
 		userGroup : 好友信息修改后添加到 userGroup 中，下次获取群成员时区分获取本地还是服务器。
@@ -213,11 +277,12 @@
 			
 		}
 	};
+
 	/**
 		data: { 'groupId':[] }
 		onChange: {groupId:'', memebers:[]}
 	*/
-	RongIM.GroupMembers = { 
+	var GroupMembers = { 
 		data : {  },
 		set:function(groupId, memebers){
 			this.data[groupId] = memebers;
@@ -225,7 +290,7 @@
 		},
 		get:function(groupId, callback){
 			if (UserRelation.getUserGroup(groupId).group.length > 0 ) {
-				RongIM.API.getGroupInfo([], groupId, function(group){
+				API.getGroupInfo([], groupId, function(group){
 					callback(group);
 				});
 			}else{
@@ -237,18 +302,15 @@
 		}
 	};
 
-	/**
-		data:数据格式:[]
-		get: callback 必传参数, groupId 可选参数
-	*/
-	RongIM.Group = {
+
+	var Group = {
 		data:[],
 		set:function(data){ },
 		get:function(callback, groupId){
 			if (typeof groupId == 'undefined') {
 				return this.data;
 			}else{
-				RongIM.API.getGroupInfo(this.data, groupId, function(group){
+				API.getGroupInfo(this.data, groupId, function(group){
 					callback(group);
 				});
 			}
@@ -256,81 +318,16 @@
 		remove:function(){},
 		onChange:function(group){}
 	};
-	
-	/**
-	getUserInfo.data 数据模型中缓存的数据，自己决定在 data 中取或在服务取
-	*/
-	RongIM.API = { 
-		getUserInfo:function(data, userId, callback){
-			if (userId in data) {
-				callback(data[userId]);
-			}else{
-				var defaultUser = {
-					name:'陌生人',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/Uz6Sw8GXx1476068767254905029'
-				};
-				var users = {'1002':{id:'1002',name:'zhaoliu',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/675NdFjkx1466733699776768066'},
-							'1003':{id:'1003',name:'wangwu',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/FjsNMjYoVKfGmA86SNwnggfKgE6_'},
-							'1004':{id:'1004',name:'lisi',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/Tp6nLyUKX1466570511241467041'},
-							'1005':{id:'1005',name:'zhangsan',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/Fh4fnCvnO_SOwpuMPYGBnzBwrx6A'},
-							'1001':{id:'1001',name:'Martin',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/u0LUuhzHm1466557920584458984'}}
-				callback(users[userId] || defaultUser);
-			}
-		},
-		getUsers:function(callback){},
-		getFriendShip : function(callback){ },
-		getGroups:function(callback){ },
-		getGroupMembers:function(callback){ },
-		getGroupInfo:function(data, groupId, callback){ 
-			if (data.length > 0) {
-				for(var i=0,len = data.length; i<len; i++){
-					if (data[i].group.id == groupId) {
-						callback(data[i]);
-						break;
-					}
-				}
-			}else{
-				callback({id:'"daGroup9901"',name:'RongCloud',portraitUri:'http://7xogjk.com1.z0.glb.clouddn.com/Tp6nLyUKX1466570097727076172'});
-			}
-		},
-		getConversationList : function(callback){ },
-		
-		sortConversationList : function(conversationList, callback){ 
-			var convers = [];
-	        for (var i = 0, len = conversationList.length; i < len; i++) {
-	            if (!conversationList[i]) {
-	                continue;
-	            }
-	            if (conversationList[i].isTop) {
-	                convers.push(conversationList[i]);
-	                conversationList.splice(i, 1);
-	                continue;
-	            }
-	            for (var j = 0; j < len - i - 1; j++) {
-	                if (conversationList[j].sentTime < conversationList[j + 1].sentTime) {
-	                    var swap = conversationList[j];
-	                    conversationList[j] = conversationList[j + 1];
-	                    conversationList[j + 1] = swap;
-	                }
-	            }
-	        }
-	        return convers.concat(conversationList);
-		},
-		removeConversation:function(conversationType, targetId, callback){},
-		getHistoryMessages : function(conversationType, targetId, timestamp, callback){ 
-			RongIMLib.RongIMClient.getInstance().getHistoryMessages(conversationType, targetId, timestamp, Util.opts.get_historyMsg_count, {
-			  onSuccess: function(list, hasMore) {
-			  	callback(list, hasMore);
-			  },
-			  onError: function(error) { }
-			});
-		}
+
+	Listener.onMessage = function(message){
+		Message.set(message);
 	};
 	/**
 	1、值存储实时消息和补偿消息（更改 getHistoryMessages 时间戳），不存储历史消息,当前会话没有历史消息和补偿消息除外
 	2、get.position 1、点击会话 2、调用历史消息接口
 	3、get.callback(list,hasMore)
 	*/
-	RongIM.Message = {
+	var Message = {
 		dataKey:'conversation_',
 		data:{},
 		set:function(message){
@@ -338,11 +335,12 @@
 			var	key = me.dataKey + message.conversationType + message.targetId;
 			Util.composeObject(message, function(result){
 				if (me.data[key]) {
-					me.data[key].unshift(result);
+					me.data[key].push(result);
 				}else{
 					me.data[key] = [message];
 				}
-				RongIM.Conversation.set(Util.createConvresation(message));
+				var conversation = RongIMClient._memoryStore.conversationList.slice(0,1)[0];
+				Conversation.set(conversation);
 				me.onChange(message);
 			});
 		},
@@ -352,11 +350,11 @@
 			var memoryMsgs = me.data[key];
 			if (position == 2 || !memoryMsgs) {
 				var timestamp = memoryMsgs ? memoryMsgs[memoryMsgs.length - 1].sentTime : 0; 
-				RongIM.API.getHistoryMessages(conversationType, targetId, timestamp, function(list, hasMore){
+				API.getHistoryMessages(conversationType, targetId, timestamp, function(list, hasMore){
 					Util.composeObject(list, function(result){
 						if (!memoryMsgs) {
 							me.data[key] = result;
-						}else if(memoryMsgs.length < Util.opts.get_historyMsg_count){
+						}else if(memoryMsgs.length < opts.get_historyMsg_count){
 							me.data[key].concat(result);
 						}
 						setTimeout(function(){
@@ -374,30 +372,19 @@
 		onChange:function(message){}
 	};
 
-	/**
 
-	callback.data : {newIdx:0, oldIdx:0, data:conversation}
-	*/
-	RongIM.Conversation = {
+	var Conversation = {
 		data : [],
 		get : function(callback){
 			var me = this; 
 			if (me.data.length == 0) {
-				RongIMClient.getInstance().getConversationList({
-				 onSuccess: function(result) {
-				  	Util.composeObject(result,function(items){
-				  		var list = RongIM.API.sortConversationList(items);
-				  		if (list.length > Util.opts.max_conversation_count) list.length = Util.opts.max_conversation_count;
-						me.data = list;
-						callback(me.data);
-					});
-				  },
-				  onError: function(error) {
-				  }
-				},[1,3]);
+				API.getConversationList(function(list){
+					me.data = list;
+					callback(me.data);
+				});
 			}else{
 				Util.composeObject(me.data,function(items){
-			  		var list = RongIM.API.sortConversationList(items);
+			  		var list = API.sortConversationList(items);
 					callback(list);
 				});
 			}
@@ -415,14 +402,14 @@
 			}
 			//TODO  setTop
 			isInsert && me.data.unshift(conversation);
-			me.onChange(me.data);
+			me.onChange(conversation);
 		},
 		clearUnReadCount:function(conversationType, targetId){ },
 		remove:function(conversationType, targetId, callback){
 			var me = this;
 			for(var i =0,len=me.data.length; i<len; i++){
 				if (me.data[i].conversationType == conversation.conversationType && me.data[i].targetId == conversation.targetId) {
-					RongIM.API.removeConversation(me.data[i], function(){
+					API.removeConversation(me.data[i], function(){
 						me.data.splice(i,1);
 						me.clearUnReadCount(conversationType, targetId);
 					});
@@ -430,15 +417,21 @@
 				}
 			}
 		},
-		onChange : function(list){ }
+		onChange : function(conversation){ }
 	};
+	
 
-	RongIM.Status = { 
+	Listener.onStatusChange = function(status){
+		Status.set(status);
+	};
+	var Status = { 
 		set : function(status){
 			this.onChange(status);
 		},
 		onChange:function(status){ }
 	};
 
-	window.RongIM = RongIM;
-})();
+	win.IM = {
+		init:init
+	};
+})(window);
