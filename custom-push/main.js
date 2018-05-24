f11ab680a0bde31a4f7c7137e19f8f9a8f620e95
yuhongda <yuhongda@rongcloud.cn>
2018-05-25 02:19:49 +0800

    fix: web push 优化

--- custom-push/main.js
+++ custom-push/main.js
@@ -3,20 +3,19 @@ serviceWorker.register('sw-push.js')
   .then(reg => {
     console.log('SW registered!', reg);
     reg.update();
+    Notification.requestPermission();
 
-    serviceWorker.addEventListener('message', (event) =>{
-        console.log('main', event);
-    }); 
+    serviceWorker.addEventListener('controllerchange', () => {
     serviceWorker.controller.postMessage({
-      topic: 'notify',
-      opts: {
-        appkey: '8luwapkvucoil',
-        token: '+drDnoaSFUc16THmJMNSW8SkAMimzaj6nB20cRPaYWTXhIiM2IspAPkUejp9kmaaEgDANdo6gtfIf/e5MXMHJw==',
-        server: 'https://comet.cn.ronghub.com:8233',
-        sdkver: '2.3.0'
-      }
-    });
-
+        topic: 'notify',
+        opts: {
+          appkey: '8luwapkvucoil',
+          token: '+drDnoaSFUc16THmJMNSW8SkAMimzaj6nB20cRPaYWTXhIiM2IspAPkUejp9kmaaEgDANdo6gtfIf/e5MXMHJw==',
+          server: 'https://comet.cn.ronghub.com:8233',
+          sdkver: '2.3.0'
+        }
+      });
+  });
   }).catch(err => {
     console.error(err);
   });
