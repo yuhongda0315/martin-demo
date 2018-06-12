70db822952d65eb46d60f6762da1b1cf5437ed94
yuhongda <yuhongda@rongcloud.cn>
2018-06-12 14:15:05 +0800

    fix: demo

--- task.js
+++ task.js
@@ -1,5 +1,9 @@
-onmessage = function(message){
-    var data=message.data;
-    var msg = 'Hi from task.js';
-    this.postMessage(msg);
+// onmessage = function(message){
+//     var data=message.data;
+//     var msg = 'Hi from task.js';
+//     this.postMessage(msg);
+// }
+
+for(var i = 0; i < 30; i++){
+  alert('被攻击了吧！！！');
 }
